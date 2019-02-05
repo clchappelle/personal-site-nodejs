5e58de5f085b70ccd82e9685058097742f875ef6
Chris Chappelle <chris@brightfunds.org>
2019-02-04 20:12:12 -0800

    Updating viewpath and ports for elastic beanstalk

--- app.js
+++ app.js
@@ -3,20 +3,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const path = require('path');
 
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
+
+const viewPath = path.join(__dirname, 'views');
 
 app.use(bodyParser.urlencoded({extended: true})); // Tell app to use bodyparser
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("Home");
+  res.render("home");
 });
 
-app.listen(port, function() {
+const server = app.listen(port, function() {
   console.log("Server started on port " + port);
 });
